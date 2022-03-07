27464dfb8dbbf0094cde56b61596712bf4dd580a
Thamas21 <53061030+Thamas21@users.noreply.github.com>
2022-03-07 10:27:46 -0800

    Added files

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import (render_template, url_for,
+                   request, redirect)
+from models import (app, Project, db)
+from datetime import datetime
+
+
+def date_strptime(date_str):
+    date_str = date_str.replace('-', ' ')
+    date_obj = datetime.strptime(date_str, '%Y %m')
+    return date_obj
+
+
+@app.route('/')
+def index():
+    projects = Project.query.all()
+    return render_template('index.html', projects=projects)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/detail/<id>')
+def detail(id):
+    project = Project.query.get_or_404(id)
+    return render_template('detail.html', project=project)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', msg=error), 404
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True, port=8000, host='127.0.0.1')
