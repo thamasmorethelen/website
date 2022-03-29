808467f2bb9801f4d61ba2b395a096d36c32768a
Thomas Thelen-Clemmons <thomasthelen-clemmons@Thomass-MacBook-Air.local>
2022-03-28 17:02:11 -0700

    application.py updates

--- application.py
+++ application.py
@@ -9,16 +9,5 @@ def index():
     return render_template('index.html')
 
 
-@application.route('/about')
-def about():
-    return render_template('about.html')
-
-
-
-@application.errorhandler(404)
-def not_found(error):
-    return render_template('404.html', msg=error), 404
-
-
 if __name__ == '__main__':
     application.run()
